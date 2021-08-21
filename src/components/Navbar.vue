081eb935b3ff9671d13e6a240cf66396c817861d
DonYarce <alejandro.yarce@udea.edu.co>
2021-08-20 21:09:50 -0500

    feat: Navbar and Myprofile update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,8 +2,13 @@
 <template>
   <nav class="flex justify-between bg-retrat shadow-md relative p-2">
     <img  src="/src/assets/icons/firework.svg" alt="firework svg">
-    <div class="flex-grow flex items-center">
-      <button class="bg-black text-white font-bold rounded-lg px-4 py-1 font-righteous tracking-widest" @click="router.push({name: 'myProfile'})">MyProfile</button>
+    <div class="flex-grow flex items-center ">
+      <div>
+        <button class="bg-black text-white font-bold rounded-lg px-4 py-1 font-righteous tracking-widest hover:bg-gray-600" @click="router.push({name: 'myProfile'})">MyProfile</button>
+      </div>
+      <div>
+        <button class="bg-black text-white font-bold rounded-lg px-4 py-1 font-righteous tracking-widest hover:bg-gray-600 ml-1" @click="router.push({name: 'home'})">Home</button>
+      </div>
     </div>
     <div class="flex items-center">
       <button class="bg-red-500 text-white font-bold rounded-lg px-4 py-1 font-righteous tracking-widest" @click="logout">SALIR</button>
