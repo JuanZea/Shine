04eae2fd40f50ef1c6669aaa4217e283aff180bb
Juan David Zea <vikzea@gmail.com>
2021-08-31 07:11:07 -0500

    refactor: Home view

---
+++ src/components/Poster.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-center">
+    <div class="bg-green-500 relative">
+      <img class="rounded-lg" width="200" height="300" :src="source" alt="Poster image">
+      <div class="absolute inset-0 h-full w-full opacity-0 bg-black hover:opacity-100 rounded-lg px-6 py-4">
+        <h1 class="font-bold text-white font-righteous mb-2">{{ title }}</h1>
+        <p class="text-white line-clamp-3">{{ desc }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {source: {required: true, type: String}, title: {required: true, type: String}, desc: {required: true, type: String}},
+  name: "Poster"
+}
+</script>
--- src/views/auth/Home.vue
+++ src/views/auth/Home.vue
@@ -3,29 +3,20 @@
     <div class="h-72 bg-orange-600">
       <carousel/>
     </div>
-    <div id="123" class="flex-grow bg-poco shadow-t relative ">
-      <div class="bg-blue-500 box-shadow flex-grow grid grid-rows-3 gap-4 float-left w-5/6 ">
-            <div class="flex row-auto justify-around">
-              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
-              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
-              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/useless-47758-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/useless-47758-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
-              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
-            </div>
-            <div class="flex row-auto justify-around">
-              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
-              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
-              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/useless-47758-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/useless-47758-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
-              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
-            </div>
-        </div>
 
-      <div class="bg-red-400 flex-grow w-1/6 grid grid-cols-1">
-        <hi class="flex justify-center">Top Calification</hi>
-        <div class="">
-            <img width="80" height="121" src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
-            <img width="80" height="121" src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
-            <img width="80" height="121" src="https://cuevana3.io/wp-content/uploads/2021/08/useless-47758-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/useless-47758-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
-            <img width="80" height="121" src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+    <div class="container mx-auto bg-poco lg:flex">
+      <div class="flex-grow">
+        <div class="bg-blue-500 py-6 gap-y-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+          <poster title="La momia" desc="En una tumba profunda debajo del extenso desierto una antigua princesa es despertada, trayendo consigo toda la maldad y el terror que fueron creciendo durante los siglos y que desafían todo entendimiento humano." source="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg"/>
+          <poster title="La momia" desc="En una tumba profunda debajo del extenso desierto una antigua princesa es despertada, trayendo consigo toda la maldad y el terror que fueron creciendo durante los siglos y que desafían todo entendimiento humano." source="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg"/>
+          <poster title="La momia" desc="En una tumba profunda debajo del extenso desierto una antigua princesa es despertada, trayendo consigo toda la maldad y el terror que fueron creciendo durante los siglos y que desafían todo entendimiento humano." source="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg"/>
+          <poster title="La momia" desc="En una tumba profunda debajo del extenso desierto una antigua princesa es despertada, trayendo consigo toda la maldad y el terror que fueron creciendo durante los siglos y que desafían todo entendimiento humano." source="https://cuevana3.io/wp-content/uploads/2021/08/useless-47758-poster-200x300.jpg"/>
+        </div>
+      </div>
+      <div class="bg-red-500 px-6">
+        <h2 class="text-2xl font-bold font-righteous mb-6">Destacadas</h2>
+        <div>
+          <poster title="La momia" desc="En una tumba profunda debajo del extenso desierto una antigua princesa es despertada, trayendo consigo toda la maldad y el terror que fueron creciendo durante los siglos y que desafían todo entendimiento humano." source="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg"/>
         </div>
       </div>
     </div>
@@ -34,8 +25,10 @@
 
 <script>
 import Carousel from "../../components/Carousel.vue";
+import Poster from "../../components/Poster.vue";
+
 export default {
   name: "Home",
-  components: {Carousel}
+  components: {Poster, Carousel}
 }
 </script>
