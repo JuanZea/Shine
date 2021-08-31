a408f4a2ac99505381ae1e8b1bdea101696c0b58
Alejandro Yarce <alejoyarce@hotmail.es>
2021-08-31 06:07:43 -0500

    Update Home: Barra derecha de Top Películas

--- src/views/auth/Home.vue
+++ src/views/auth/Home.vue
@@ -3,17 +3,33 @@
     <div class="h-72 bg-orange-600">
       <carousel/>
     </div>
-    <div class="flex-grow bg-poco shadow-t relative ">
-      <div class="bg-poco box-shadow m-10 flex-grow">
-          <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+    <div id="123" class="flex-grow bg-poco shadow-t relative ">
+      <div class="bg-blue-500 box-shadow flex-grow grid grid-rows-3 gap-4 float-left w-5/6 ">
+            <div class="flex row-auto justify-around">
+              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/useless-47758-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/useless-47758-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+            </div>
+            <div class="flex row-auto justify-around">
+              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/useless-47758-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/useless-47758-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+              <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+            </div>
         </div>
 
-
-
-        <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
-        <img width="160" height="242" src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+      <div class="bg-red-400 flex-grow w-1/6 grid grid-cols-1">
+        <hi class="flex justify-center">Top Calification</hi>
+        <div class="">
+            <img width="80" height="121" src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/righteous-blood-47392-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+            <img width="80" height="121" src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/seven-days-war-47403-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+            <img width="80" height="121" src="https://cuevana3.io/wp-content/uploads/2021/08/useless-47758-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/08/useless-47758-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+            <img width="80" height="121" src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" data-src="https://cuevana3.io/wp-content/uploads/2021/04/ironbark-el-espia-ingles-41153-poster-200x300.jpg" class="lazy attachment-thumbnail size-thumbnail wp-post-image" alt="img">
+        </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
