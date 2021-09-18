bc1f966987c9017e65ef63be0f331e55d6ad6f0e
Juan David Zea <vikzea@gmail.com>
2021-09-18 13:59:18 -0500

    feat: Searchbar

--- src/use/useFilms.js
+++ src/use/useFilms.js
@@ -1,15 +1,24 @@
-import {ref} from "vue";
-import {filmsService} from "../services/filmsService";
+import { ref } from "vue";
+import { filmsService } from "../services/filmsService";
+import debounce from "lodash/debounce";
 
 const films = ref();
+const options = ref(null);
 
-const refreshFilms = () => filmsService.all().then(response => {
-    films.value = response;
-});
+const refreshFilms = debounce(async () => {
+    films.value = await filmsService.all(options.value);
+}, 200);
+
+const setQuery = (query) => {
+    if (!query) options.value = null;
+    else options.value = { params: {contains: query}}
+}
 
 export default function useFilms() {
     return {
         films,
-        refreshFilms
+        refreshFilms,
+        setQuery,
+        options
     }
 }
